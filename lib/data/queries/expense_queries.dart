abd12b73b834d7879440baff2e9e6be2ae253299
raul94a <raul200794@gmail.com>
2023-01-31 22:37:54 +0100

    Fix read expenses queries

--- lib/data/queries/expense_queries.dart
+++ lib/data/queries/expense_queries.dart
@@ -6,7 +6,7 @@ class ExpenseQueries {
     const limit = 100;
     String sql = '';
     sql =
-        'SELECT e.*, date(e.createdDate / 1000 , "unixepoch", "localtime") as "date" from $_table e where deleted = 0 AND isCommonExpense = 1 order by createdDate desc LIMIT $limit OFFSET $offset';
+        'SELECT e.*, date(e.createdDate / 1000 , "unixepoch", "localtime") as "date" from $_table e where deleted = 0 order by createdDate desc LIMIT $limit OFFSET $offset';
    
     // print(sql);
     return sql;
@@ -23,14 +23,14 @@ class ExpenseQueries {
     const limit = 100;
     String sql = '';
     sql =
-        'SELECT e.*, date(e.createdDate / 1000 , "unixepoch", "localtime") as "date" from $_table e where deleted = 0 AND isCommonExpense = 1 AND personFirebaseUID = "$id" order by createdDate desc LIMIT $limit OFFSET $offset';
+        'SELECT e.*, date(e.createdDate / 1000 , "unixepoch", "localtime") as "date" from $_table e where deleted = 0 AND isCommonExpense = 0 AND personFirebaseUID = "$id" order by createdDate desc LIMIT $limit OFFSET $offset';
    
     // print(sql);
     return sql;
   }
    static String readExpensesOfUserByDateIndividual(String id){
     String sql =
-        'SELECT SUM(price) as "total" from $_table e where deleted = FALSE AND isCommonExpense = 1 AND personFirebaseUID = "$id"';
+        'SELECT SUM(price) as "total" from $_table e where deleted = 0 AND isCommonExpense = 0 AND personFirebaseUID = "$id"';
       
     return sql;
   }
